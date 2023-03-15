d99556b85c7d7abbfcf5a4ca9fc9e46f107c698c
Guilherme Amorim <guilhermedeamorim098@gmail.com>
2023-03-15 11:59:46 -0300

    TASK - CRUD Postagem

--- src/main/java/br/com/gen/guilh/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/br/com/gen/guilh/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,16 @@
 package br.com.gen.guilh.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.gen.guilh.blogpessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
